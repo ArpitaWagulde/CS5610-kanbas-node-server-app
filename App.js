1958523e4860951b65ceb2a232ec0168a54ef641
ArpitaWagulde <arpitawgulde@gmail.com>
2024-04-18 20:22:13 -0400

    A6 setup

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 
 import cors from "cors";
 const app = express();
@@ -15,3 +17,7 @@ AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
+mongoose.connect(
+  "mongodb+srv://Cluster78574:WV9LRFhrU0lf@cluster78574.bwulyn0.mongodb.net/kanbas"
+);
+UserRoutes(app);
