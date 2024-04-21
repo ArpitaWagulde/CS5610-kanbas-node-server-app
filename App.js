bf87429be5240d0c2ecce576f6aedc2e134cfecd
ArpitaWagulde <arpitawgulde@gmail.com>
2024-04-20 23:29:57 -0400

    Reverts

--- App.js
+++ App.js
@@ -4,14 +4,9 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-import mongoose from "mongoose";
-import UserRoutes from "./Users/routes.js";
 
 import cors from "cors";
 
-mongoose.connect(
-  "mongodb+srv://Cluster78574:WV9LRFhrU0lf@cluster78574.bwulyn0.mongodb.net/kanbas"
-);
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -22,7 +17,3 @@ QuizRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
-mongoose.connect(
-  "mongodb+srv://Cluster78574:WV9LRFhrU0lf@cluster78574.bwulyn0.mongodb.net/kanbas"
-);
-UserRoutes(app);
