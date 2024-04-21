88281cfa20d31b9ffa556157a18d363e43b5594e
ArpitaWagulde <arpitawgulde@gmail.com>
2024-04-21 14:20:57 -0400

    Accounts updates

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import QuizRoutes from "./Kanbas/quizzes/routes.js";
-
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 import cors from "cors";
 
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 const app = express();
 app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 const sessionOptions = {
