94b8a216e59968370104143b2a93d424de155855
ArpitaWagulde <arpitawgulde@gmail.com>
2024-04-21 14:15:59 -0400

    Updates of Mongo

--- App.js
+++ App.js
@@ -9,7 +9,21 @@ import QuizRoutes from "./Kanbas/quizzes/routes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
@@ -18,3 +32,4 @@ QuizRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
+UserRoutes(app);
